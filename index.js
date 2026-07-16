740b05f3476da26b6cd004453a8ee66052bc89b3
Anamulhaque01 <anamulhaque0357@gmail.com>
2026-07-16 09:12:20 +0600

    Client Url Updated

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const port = process.env.PORT || 5000;
 // ==========================================
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://ph-assignment-client-09.vercel.app' // Replace with your frontend domain when ready
+    'https://ph-assignment-client-09.vercel.app', // Replace with your frontend domain when ready
+    'https://doc-appoint-client-five.vercel.app'
 ];
 
 app.use(cors({
